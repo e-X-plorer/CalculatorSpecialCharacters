5fba7b2124b107ba3bfa7d433e57bed596644c1d
e-X-plorer <arsenicpool3d@gmail.com>
2020-03-24 19:09:50 +0300

    Added constant strings representing numeric values and methods for their interpretation.

--- Constants.cs
+++ Constants.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CalculatorSpecialCharacters
 {
-    class Program
+    static class Constants
     {
-        static void Main(string[] args)
+        private static Dictionary<string, double> specialStrings= new Dictionary<string, double>()
         {
-            Console.WriteLine("Hello World!");
+            ["pi"] = 3.14159265,
+            ["e"] = 2.71828183
+        };
+
+        private static List<Dictionary<string, double>> cache;
+
+        private static void Init()
+        {
+            cache = new List<Dictionary<string, double>>();
+            foreach (var entry in specialStrings)
+            {
+                int index = entry.Key.Length - 1;
+                if (index < 0)
+                {
+                    Console.WriteLine("Empty string representing {0} found and skipped during initialization!", entry.Value);
+                    continue;
+                }
+                if (cache[index] == null)
+                {
+                    cache.Capacity = index;
+                }
+                cache[index].Add(entry.Key, entry.Value);
+            }
+        }
+
+        private static void SubstituteConstantsSinglePass(StringBuilder input, int pass)
+        {
+            foreach (var entry in cache[pass])
+            {
+                input.Replace(entry.Key, entry.Value.ToString());
+            }
+            
+        }
+
+        public static string SubstituteConstants(string input)
+        {
+            if (cache == null)
+            {
+                Init();
+            }
+            StringBuilder inputBuilder = new StringBuilder(input);
+            for (int i = cache.Count - 1; i >= 0; i--)
+            {
+                SubstituteConstantsSinglePass(inputBuilder, i);
+            }
+            return inputBuilder.ToString();
         }
     }
 }
