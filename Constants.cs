ca44d4e04a3cef32f6de64f8dc1e074dadf12f82
e-X-plorer <arsenicpool3d@gmail.com>
2020-03-24 17:43:34 +0300

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorSpecialCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
